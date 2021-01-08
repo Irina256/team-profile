73c709f803233e13cb55fd938bb76b5dde393c2f
Irina Skitayeva <irina.skitayeva@gmail.com>
2021-01-08 01:33:34 -0500

    added styles

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const inquirer = require("inquirer");
-const fs = require("fs");
+
 const generatePage = require("./src/page-template.js");
 
 const promptManager = () => {
   return inquirer.prompt([
     {
       type: "input",
-      name: "name",
+      name: "name1",
       message: "What is your team manager’s name?",
       validate: (nameInput) => {
         if (nameInput) {
@@ -19,7 +20,7 @@ const promptManager = () => {
     },
     {
       type: "input",
-      name: "ID",
+      name: "id",
       message: "What is your employee ID?",
     },
     {
@@ -34,10 +35,19 @@ const promptManager = () => {
     },
   ]);
 };
-const promptNewEmployee = (data) => {
-  if (!data.positions) {
-    data.positions = [];
+
+const promptTeam = (teamData) => {
+  console.log(`
+=================
+Add Engineer or Intern
+=================
+`);
+
+  // If there's no 'projects' array property, create one
+  if (!teamData.members) {
+    teamData.members = [];
   }
+
   return inquirer
     .prompt([
       {
@@ -45,76 +55,67 @@ const promptNewEmployee = (data) => {
         name: "positions",
         message:
           "Who would you like to add next?(Use arrow Down/Up keys to select)",
-        choices: ["Engineer", "Intern", "Noone"],
+        choices: ["Engeneer", "Intern", "Noone"],
+      },
+      {
+        type: "input",
+        name: "name2",
+        message: "Please provide member's first name",
+      },
+      {
+        type: "input",
+        name: "id",
+        message: "What is his ID?",
+      },
+      {
+        type: "input",
+        name: "email",
+        message: "What is your team member's email address?",
+      },
+      {
+        type: "input",
+        name: "github",
+        message: "What is team member's github?",
+      },
+      {
+        type: "confirm",
+        name: "confirmAddMember",
+        message: "Would you like to add another member?",
+        default: false,
       },
     ])
-    .then((answer) => {
-      // data.positions.push(answer);
-      if (answer.positions === "Engineer") {
-        promptEngineer();
-      } else if (answer.positions === "Intern") {
-        console.log("1");
-        promptIntern();
+    .then((memberInfo) => {
+      teamData.members.push(memberInfo);
+      if (memberInfo.confirmAddMember) {
+        return promptTeam(teamData);
       } else {
+        return teamData;
       }
     });
 };
 
-const promptEngineer = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your engineer's name?",
-      },
-      {
-        type: "input",
-        name: "ID",
-        message: "What is your engineer's ID?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your engineer's email address?",
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "What is your engineer's github?",
-      },
-    ])
-    .then(promptNewEmployee);
-};
+promptManager()
+  .then(promptTeam)
+  .then((teamdata) => {
+    return generatePage(teamdata);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+return copyFile();
+// .then((data)=>{
+//   return generateHTML(data);
 
-const promptIntern = () => {
-  return inquirer
-    .prompt([
-      {
-        type: "input",
-        name: "name",
-        message: "What is your intern's name?",
-      },
-      {
-        type: "input",
-        name: "ID",
-        message: "What is your intern's ID?",
-      },
-      {
-        type: "input",
-        name: "email",
-        message: "What is your intern's email address?",
-      },
-      {
-        type: "input",
-        name: "github",
-        message: "What is your intern's github?",
-      },
-    ])
-    .then(promptNewEmployee);
-};
-
-promptManager().then(promptNewEmployee);
 // .then((answer) => {
 //   if (answer.positions[0]) {
 //     promptEngineer();
--- src/page-template.js
+++ src/page-template.js
@@ -1,21 +1,99 @@
+const generateIcon = (positions) => {
+  if (positions === "Engineer") {
+    return `<p><i class="fas fa-glasses"></i> ${positions}</p>`;
+  } else if (positions === "Intern") {
+    return `<p><i class="fas fa-user-graduate"></i> ${positions}</p>`;
+  }
+};
+const generateMembers = (membersArr) => {
+  return `
+             ${membersArr
+               .map(({ name2, id, email, github, positions }) => {
+                 return `
+               
+                 <div class="col-sm-4">
+                 <div class="card" style="width: 22rem;">
+            <h2 class="card-header">
+            ${name2}
+           ${generateIcon(positions)}
+              </h2>
+                    <div class="card-body">
+                      <div class="card" style="width: 18rem;">
+                        <ul class="list-group list-group-flush">
+                          <li class="list-group-item">ID: ${id}</li>
+                          <li class="list-group-item">Email: <a href= "mailto:${email}">${email}</a></li>
+                          <li class="list-group-item">GitHub: <a href= "https://github.com/${github}">${github}</a></li>
+                        </ul>
+                      </div>
+            </div>
+          </div>
+          </div>
+          `;
+               })
+
+               .join("")}
+  
+        
+    `;
+};
+
 module.exports = (templateData) => {
-  console.log(templateData);
+  const { members, ...manager } = templateData;
 
   return `
-      <!DOCTYPE html> 
-      <html lang="en"> 
-      <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Team Profile</title>
-      </head>
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Team Profile</title>
+    <link rel="stylesheet" href ="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+  <header>
+  <div class="container flex-row justify-space-between align-center py-3">
+    <h1 class="page-title">My Team</h1>
+   
+  </div>
+</header>
+<main class="container my-5">
+<div class="row">
+  <div class="col-sm-4">
+  <div class="card" style="width: 22rem;">
+    <h2 class="card-header">
+    ${manager.name1}
+    <p><i class="fas fa-mug-hot"></i> Manager</p>
+      </h2>
+            <div class="card-body">
+              <div class="card" style="width: 18rem;">
+                <ul class="list-group list-group-flush">
+                  <li class="list-group-item">ID: ${manager.id}</li>
+                  <li class="list-group-item">Email: <a href="mailto:${
+                    manager.email
+                  }">${manager.email}</a></li>
+                  <li class="list-group-item">Office Number: ${
+                    manager.office
+                  }</li>
+                </ul>
+              </div>
+    </div>
+  </div>
+  </div>
+       ${generateMembers(members)}
+    </main>
     
-      <body>
-      <title>My Team</title>
-        <h1>${name}</h1>
-        <h2><a href="https://github.com/${github}">Github</a></h2>
-      </body>
-      </html>
-      `;
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${
+    manager.name1
+  }</h3>
+    </footer>
+  </body>
+  </html>
+  `;
 };
